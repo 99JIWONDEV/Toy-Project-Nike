5052def6575094d5bd4d5b9f5804970d518cacb1
99JIWONDEV <99jiwonpark@gmail.com>
2024-03-06 21:56:29 +0900

    app.jsx 컴포넌트화

---
+++ src/section/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero'
+import PopularProducts from './PopularProducts'
+import SuperQuality from './SuperQuality'
+import Footer from './Footer'
+import Services from './Services'
+import SpecialOffer from './SpecialOffer'
+import Subscribe from './Subscribe'
+import CustomerReviews from './CustomerReviews'
+
+export {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Footer,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  CustomerReviews
+}
