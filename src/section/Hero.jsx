f0d7dafdf3cec65d589d4457eefd0226076e8c27
99JIWONDEV <99jiwonpark@gmail.com>
2024-03-07 11:00:38 +0900

    shoecard 제작

--- src/section/Hero.jsx
+++ src/section/Hero.jsx
@@ -1,9 +1,13 @@
 import { arrowRight } from "../assets/icons";
 import Button from "../components/Button";
 import { shoes, statistics } from "../constants";
-import {bigShoe1} from "../assets/images"
+import { bigShoe1 } from "../assets/images";
+import ShoeCard from "../components/ShoeCard";
+import { useState } from "react";
 
 const Hero = () => {
+  const [bigShoeImg, seBigShoeImg] = useState(bigShoe1);
+
   return (
     <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen max-container gap-10">
       <div className=" relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
@@ -14,22 +18,28 @@ const Hero = () => {
           <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
         </h1>
         <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">Discover stylish Nike arrivals, quality comfort, and innovation for your active life.</p>
-        <Button label = "Show Now" iconURL ={arrowRight}/>
+        <Button label="Show Now" iconURL={arrowRight} />
         <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
-        {statistics.map((stat) => (
-          <div key={stat.label}>
-            <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
-            <p className=" leading-7 font-montserrat text-slate-gray">{stat.label}</p>
-          </div>
-        ))}
+          {statistics.map((stat) => (
+            <div key={stat.label}>
+              <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
+              <p className=" leading-7 font-montserrat text-slate-gray">{stat.label}</p>
+            </div>
+          ))}
         </div>
       </div>
       <div className=" relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
-        <img src={bigShoe1} alt="" width={610} height={500} className=" object-contain relative z-10" />
-        <div>
-          {shoes.map((shoe)=> (
+        <img src={bigShoeImg} alt="" width={610} height={500} className=" object-contain relative z-10" />
+        <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
+          {shoes.map((shoe) => (
             <div key={shoe}>
-              <shoeCard/>
+              <ShoeCard
+                imgURL={shoe}
+                changeBigShoeImage={(shoe) => {
+                  seBigShoeImg(shoe);
+                }}
+                bigShoeImg={bigShoeImg}
+              />
             </div>
           ))}
         </div>
